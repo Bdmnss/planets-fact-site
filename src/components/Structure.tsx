081189532fe7d8f114818f3f76bd3f9bfc31fc29
Bdmnss <lukabadali123@gmail.com>
2024-07-15 18:37:30 +0400

    add structure desktop size

--- src/components/Structure.tsx
+++ src/components/Structure.tsx
@@ -18,6 +18,10 @@ interface PlanetData {
       width: string;
       height: string;
     };
+    desktop: {
+      width: string;
+      height: string;
+    };
   };
   buttonColor: string;
   rotation: string;
@@ -40,39 +44,55 @@ const Structure: React.FC<StructureProps> = ({
   screenWidth,
 }) => {
   return (
-    <div className="flex flex-col justify-center items-center px-[2.4rem] sm:px-[3.9rem]">
-      <img
-        src={planet.images.internal}
-        alt="planet image"
-        style={
-          screenWidth > 640
-            ? {
-                width: planet.planetSize.tablet.width,
-                height: planet.planetSize.tablet.height,
-              }
-            : screenWidth > 0
-            ? {
-                width: planet.planetSize.mobile.width,
-                height: planet.planetSize.mobile.height,
-              }
-            : undefined
-        }
-        className="mb-[9.8rem] mt-[9.5rem] sm:mb-[13rem] sm:mt-[14.6rem]"
-      />
-      <div className="sm:flex sm:items-center sm:gap-[6.9rem]">
-        <div className="flex flex-col justify-center items-center sm:items-start sm:w-[50%]">
-          <h2 className="text-[4rem] text-white font-antonio font-medium mb-[1.6rem]">
+    <div
+      className="flex flex-col justify-center items-center px-[2.4rem] sm:px-[3.9rem] 
+    lg:flex-row lg:mt-[12.6rem] lg:mb-[6rem]"
+    >
+      <div className="lg:w-[65%] flex justify-center">
+        <img
+          src={planet.images.internal}
+          alt="planet image"
+          style={
+            screenWidth >= 1024
+              ? {
+                  width: planet.planetSize.desktop.width,
+                  height: planet.planetSize.desktop.height,
+                }
+              : screenWidth >= 640
+              ? {
+                  width: planet.planetSize.tablet.width,
+                  height: planet.planetSize.tablet.height,
+                }
+              : screenWidth > 0
+              ? {
+                  width: planet.planetSize.mobile.width,
+                  height: planet.planetSize.mobile.height,
+                }
+              : undefined
+          }
+          className="mb-[9.8rem] mt-[9.5rem] sm:mb-[13rem] sm:mt-[14.6rem]"
+        />
+      </div>
+      <div
+        className="sm:flex sm:items-center sm:gap-[6.9rem] lg:flex-col lg:w-[35%] lg:gap-[0rem] 
+      lg:pr-[15rem]"
+      >
+        <div className="flex flex-col justify-center items-center sm:items-start sm:w-[50%] lg:w-[100%]">
+          <h2
+            className="text-[4rem] text-white font-antonio font-medium mb-[1.6rem] lg:text-[5rem]
+          xl:text-[7rem]"
+          >
             {planet.name.toUpperCase()}
           </h2>
           <p
             className="text-center text-[1.3rem] text-[#838391] leading-[2.2rem] mb-[3.2rem] 
-          sm:text-start md:text-[1.5rem]"
+          sm:text-start md:text-[1.5rem] lg:text-[2rem] lg:leading-[2.5rem]"
           >
             {planet.structure.content}
           </p>
           <p
             className="flex items-center text-[#838391] text-[1.2rem] gap-2 mb-[2.8rem] 
-          sm:text-[1.6rem]"
+          sm:text-[1.6rem] lg:text-[2rem]"
           >
             Source :
             <a
@@ -85,9 +105,9 @@ const Structure: React.FC<StructureProps> = ({
             </a>
           </p>
         </div>
-        <div className="mini:hidden sm:block w-[50%] gap-[1.6rem]">
+        <div className="mini:hidden sm:block w-[50%] gap-[1.6rem] lg:w-[100%]">
           <div
-            className="pl-[2rem] py-[1rem] border-[1px] border-[#838391] mb-[1.6rem]"
+            className="pl-[2rem] py-[1rem] border-[1px] border-[#838391] mb-[1.6rem] lg:py-[1.3rem]"
             onClick={() => handleActiveOption("overview")}
           >
             <p className="text-[1.3rem] text-white font-bold tracking-[1.93px]">
@@ -104,7 +124,7 @@ const Structure: React.FC<StructureProps> = ({
                 ? { backgroundColor: planet.buttonColor, border: "none" }
                 : undefined
             }
-            className="pl-[2rem] py-[1rem] border-[1px] border-[#838391] mb-[1.6rem]"
+            className="pl-[2rem] py-[1rem] border-[1px] border-[#838391] mb-[1.6rem] lg:py-[1.3rem]"
             onClick={() => handleActiveOption("structure")}
           >
             <p className="text-[1.3rem] text-white font-bold tracking-[1.93px]">
@@ -116,7 +136,7 @@ const Structure: React.FC<StructureProps> = ({
           </div>
 
           <div
-            className="pl-[2rem] py-[1rem] border-[1px] border-[#838391] mb-[1.6rem]"
+            className="pl-[2rem] py-[1rem] border-[1px] border-[#838391] mb-[1.6rem] lg:py-[1.3rem]"
             onClick={() => handleActiveOption("surface")}
           >
             <p className="text-[1.3rem] text-white font-bold tracking-[1.93px]">
